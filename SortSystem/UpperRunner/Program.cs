6f2b56e37f8a1ea5f45a9f40072158eb68ddc741
Bill Lee <bill@BilldeiMac.local>
2022-04-07 13:24:19 +0800

    add parameter to allow faster test

--- SortSystem/UpperRunner/Program.cs
+++ SortSystem/UpperRunner/Program.cs
@@ -18,13 +18,19 @@ logger.Fatal("Fatal Message");
 
 var rpc_port = 13456;
 var udp_port = 13457;
+var keepAlive = 3000;
 if (args.Length > 0)
 {
     rpc_port = int.Parse(args[0]);
     udp_port = int.Parse(args[1]);
+    if (args.Length > 2)
+    {
+        keepAlive = int.Parse(args[2]);
+    }
 }
 
 var uppDiscoverService = new UDPDiscoveryService(rpc_port,udp_port,"upperDiscovery");
+uppDiscoverService.KeepAliveInterval = keepAlive;
 uppDiscoverService.EndPointDiscoverFound += (object sender, DiscoverFoundEventArgs e) =>
 {
     logger.Info("Event catched "+e.ipAddr +" "+ e.rpcPort);
@@ -32,6 +38,7 @@ uppDiscoverService.EndPointDiscoverFound += (object sender, DiscoverFoundEventAr
 uppDiscoverService.StartListen();
 
 var CamDiscoverService = new UDPDiscoveryService(rpc_port+1,udp_port+1,"CamDiscovery");
+CamDiscoverService.KeepAliveInterval = keepAlive;
 CamDiscoverService.EndPointDiscoverFound += (object sender, DiscoverFoundEventArgs e) =>
 {
     logger.Info("Event catched "+e.ipAddr +" "+ e.rpcPort);
