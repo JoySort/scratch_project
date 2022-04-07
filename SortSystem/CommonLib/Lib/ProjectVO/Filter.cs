ca6f2677a7c75d9bcde661b0fdc52ad1c51fd754
Bill Lee <bill@BilldeiMac.local>
2022-04-08 00:03:46 +0800

    add config and fix typo

--- SortSystem/CommonLib/Lib/ProjectVO/Filter.cs
+++ SortSystem/CommonLib/Lib/ProjectVO/Filter.cs
@@ -5,20 +5,20 @@ public class Filter
     /**
      * <summary>Directly from json, UI pass filterRangeIndex directly</summary>
      */
-    public int[] FilterBoundrryIndeces => filterBoundrryIndeces;
+    public int[] FilterBoundrryIndices => filterBoundrryIndices;
     public Criteria Criteria => _criteria;
     public float[][] FilterBoundaries => filterBoundaries;
     
-    private int[] filterBoundrryIndeces;
+    private int[] filterBoundrryIndices;
     /**
      * 这里界限设置为2维数组，原因是为了支持潜在的一个分选标准里面选择2个区间，比如 width 0,10,20,30,100  选择0，1区间，则是0，10-10-20 理论上目前还没有这个需要，所以，二维数组的第一个维度只有一个区间是选中的。
      */
     private float[][] filterBoundaries;
     private Criteria _criteria;
 
-    public Filter(int[] boundrryIndeces, Criteria criteria)
+    public Filter(int[] boundrryIndices, Criteria criteria)
     {
-        filterBoundrryIndeces = boundrryIndeces;
+        filterBoundrryIndices = boundrryIndices;
         _criteria = criteria;
         filterBoundaries = fillBoundries().ToArray();
 
@@ -29,23 +29,23 @@ public class Filter
     {
         List<float[]> temp_boundaries = new List<float[]>();
         
-        for (var i =0 ; i< filterBoundrryIndeces.Length;i++)
+        for (var i =0 ; i< filterBoundrryIndices.Length;i++)
         {
-            if (filterBoundrryIndeces[i] == 0)
+            if (filterBoundrryIndices[i] == 0)
             {
                 float[] boundry = {_criteria.Min, _criteria.Boundaries[0]};
                 temp_boundaries.Add(boundry);
             }
-            else if(filterBoundrryIndeces[i] == _criteria.Boundaries.Length)
+            else if(filterBoundrryIndices[i] == _criteria.Boundaries.Length)
             {
-                float[] boundry = { _criteria.Boundaries[filterBoundrryIndeces[i]-1],_criteria.Max};
+                float[] boundry = { _criteria.Boundaries[filterBoundrryIndices[i]-1],_criteria.Max};
                 temp_boundaries.Add(boundry);
             }
             else
             {
-                if (filterBoundrryIndeces[i] > _criteria.Boundaries.Length)
-                    throw new Exception("Filter was out of bound 通道过滤规则选择配置超过分选条件:"+_criteria.Name+"总共只有"+_criteria.Boundaries.Length+"个区间，目前选择了"+filterBoundrryIndeces[i]);
-                float[] boundry = { _criteria.Boundaries[filterBoundrryIndeces[i]-1],_criteria.Boundaries[filterBoundrryIndeces[i]]};
+                if (filterBoundrryIndices[i] > _criteria.Boundaries.Length)
+                    throw new Exception("Filter was out of bound 通道过滤规则选择配置超过分选条件:"+_criteria.Name+"总共只有"+_criteria.Boundaries.Length+"个区间，目前选择了"+filterBoundrryIndices[i]);
+                float[] boundry = { _criteria.Boundaries[filterBoundrryIndices[i]-1],_criteria.Boundaries[filterBoundrryIndices[i]]};
                 temp_boundaries.Add(boundry);
             }
             
