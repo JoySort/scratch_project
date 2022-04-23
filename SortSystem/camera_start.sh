150cf9fad67ebed7fc17638a4d082772f0a973ce
Bill Lee <bill@BilldeiMac.local>
2022-04-23 21:43:19 +0800

    update startup script

--- SortSystem/camera_start.sh
+++ SortSystem/camera_start.sh
@@ -1,2 +1,2 @@
- #!/bin/bash
- ./CameraRunner/bin/Debug/net6.0/CameraRunner --config_folder=../../../../runner_config/camera_config
+#!/bin/bash
+./CameraRunner/bin/Debug/net6.0/CameraRunner --config_folder=../../../../runner_config/camera_config
