1a8fc8b31708e54db28b243906982ae9c95ff2e9
Bill Lee <bill@BilldeiMac.local>
2022-04-25 08:18:41 +0800

    move upper apis to upper only from commonlib

--- SortSystem/UpperRunner/Controllers/SortContoller.cs
+++ SortSystem/UpperRunner/Controllers/SortContoller.cs
@@ -106,6 +106,41 @@ public class SortController: ControllerBase
 
         return new UIAPIResult(errorObj,result);
     }
+    
+    [HttpGet]
+    [Route("/apis/discover")]
+    public UIAPIResult discover()
+    {
+        var errorObj = new JoyError();
+        var resultData = new V1Discover();
+        
+        return new UIAPIResult(errorObj,resultData);
+    }
+    
+    [HttpGet]
+    [Route("/apis/sync_status")]
+    public UIAPIResult syncStatus()
+    {
+        var errorObj = new JoyError();
+        var status = "";
+        switch (ProjectManager.getInstance().ProjectState)
+        {
+            case ProjectState.start:
+                status = "running";
+                break;
+            case ProjectState.stop:
+                status = "ready";
+                break;
+            default:
+                status = Enum.GetName(ProjectManager.getInstance().ProjectState);
+                break;
+        }
+
+        var resultData = new SynStatus(status);
+        
+        return new UIAPIResult(errorObj,resultData);
+    }
+
 }
 public class UIResultDataTotalRuntime:IJoyResult
 {
@@ -145,4 +180,37 @@ public class UIResultChannelCounter:IJoyResult
     }
     
     
+}
+
+
+public class V1Discover
+{
+    private string _node ="master";
+    private string _count = "1";
+
+    public string node
+    {
+        get => _node;
+        set => _node = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public string count
+    {
+        get => _count;
+        set => _count = value ?? throw new ArgumentNullException(nameof(value));
+    }
+}
+
+public class SynStatus
+{
+    public string sync_status
+    {
+        get;
+        set;
+    }
+
+    public SynStatus(string syncStatus)
+    {
+        sync_status = syncStatus;
+    }
 }
