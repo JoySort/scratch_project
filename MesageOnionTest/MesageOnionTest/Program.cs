5c52d236b2c64c6539a6f28a1fd1c2e5e942167a
Bill Lee <bill@BilldeiMac.local>
2022-06-26 07:24:57 +0800

    blank project

--- MesageOnionTest/MesageOnionTest/Program.cs
+++ MesageOnionTest/MesageOnionTest/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddGrpc(options =>
 {
     options.EnableDetailedErrors = true;
-    options.MaxReceiveMessageSize = 200 * 1024 * 1024; // 2 MB
+    options.MaxReceiveMessageSize = 200 * 1024 * 1024; // 200 MB
     options.MaxSendMessageSize = 5 * 1024 * 1024; // 5 MB
     
 });
@@ -27,10 +27,7 @@ app.UseEndpoints(endpoints =>
 {
     // Replace to this line instead of MapGrpcService<GreeterService>()
     endpoints.MapMagicOnionService();
-    endpoints.MapGet("/", async context =>
-    {
-        await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-    });
+
 });
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
